0e649a71e77285cddd388223e5a238f51726f818
Regina Zhyldyzbekova <regina.zhyldyzbekova@gmail.com>
2022-08-02 22:51:06 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function convertCelsiusFahrenheit (celsius){
+    return celsius * 1.8 + 32
+}
+
+console.log(convertCelsiusFahrenheit(0))
+
+const convertCelsiusFahrenheit = (celsius) =>{
+    return celsius * 1.8 + 32
+}
+console.log(convertCelsiusFahrenheit(0))
